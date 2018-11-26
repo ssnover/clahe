dc7ad3fbbe154179d442c28e677bcdc016949649
ssnover95 <sws7379@rit.edu>
2018-11-25 21:46:32 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/io/jpeg_io.hpp>
+
+int main()
+{
+    std::string const filename("sqrt_boats.jpg");
+
+
+
+    return 0;
+}
---
+++ opencv-clahe.cpp
@@ -0,0 +1,51 @@
+/*
+ * file: opencv-clahe.cpp
+ * purpose: Small application which takes an input image and applies OpenCV's
+ *          histogram equalization implementation to the image.
+ */
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+
+int main(int argc, char ** argv)
+{
+    std::string inputImageFilename;
+
+    if (argc >= 2)
+    {
+        inputImageFilename = argv[1];
+    }
+    else
+    {
+        return 2;
+    }
+
+    auto image = cv::imread(inputImageFilename);
+
+    if (image.empty())
+    {
+        std::cout << "Unable to open the image: " << inputImageFilename << std::endl;
+        return 1;
+    }
+
+    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+
+    cv::Mat equalizedImage;
+    cv::equalizeHist(image, equalizedImage);
+
+    std::string const windowNameOriginalImage("Original Image");
+    std::string const windowNameNewImage("Histogram Equalized Image");
+
+    cv::namedWindow(windowNameOriginalImage, cv::WINDOW_NORMAL);
+    cv::namedWindow(windowNameNewImage, cv::WINDOW_NORMAL);
+
+    cv::imshow(windowNameOriginalImage, image);
+    cv::imshow(windowNameNewImage, equalizedImage);
+
+    cv::waitKey(0);
+
+    cv::destroyAllWindows();
+
+    return 0;
+}
